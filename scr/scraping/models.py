fd5ade1788d0bc53199ded7501ca8ea4480490eb
ITroschenkov <qazzzzzaq@mail.ru>
2021-03-04 12:14:24 +0300

    19 video

--- scr/scraping/models.py
+++ scr/scraping/models.py
@@ -3,8 +3,8 @@ from django.db import models
 # Create your models here.
 class City(models.Model):
     name = models.CharField(max_length=25,
-    verbose_name='Название населенного пункта')
-    slug = models.CharField(max_length=25, blank=True)
+    verbose_name='Название населенного пункта', unique=True)
+    slug = models.CharField(max_length=25, blank=True, unique=True)
     
     class Meta():
     
@@ -12,4 +12,16 @@ class City(models.Model):
         verbose_name_plural='Названия населенных пунктов'
     def __str__(self):
         return self.name
-    
+
+
+class Language(models.Model):
+    name = models.CharField(max_length=25,
+                            verbose_name='Язык програмирования', unique=True)
+    slug = models.CharField(max_length=25, blank=True)
+
+    class Meta():
+        verbose_name = 'Язык програмирования'
+        verbose_name_plural = 'Языки програмирования'
+
+    def __str__(self):
+        return self.name
