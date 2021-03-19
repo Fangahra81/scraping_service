456f34da8127e3c51241a269019284b15708940e
Ilya aka Fangahra <fangahra@ya.ru>
2021-03-19 17:58:44 +0300

    19.03

--- scr/scraping/views.py
+++ scr/scraping/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from .models import Vacancy, Language
 # Create your views here.
+def home_view (request):
+    qs = Vacancy.objects.all()
+    lg = Language.objects.all()
+    return render(request, 'home.html', {'object_list': qs},{'object_lis2': lg})
